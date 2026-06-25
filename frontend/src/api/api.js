c62ed23092af795bcd17d71be93e07d7f1ebc619
tuilahieu <hieuquahay123@gmail.com>
2026-06-25 17:14:10 +0700

    change domain demo

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: "/",
+  baseURL: "/api",
 });
 
 api.interceptors.request.use((config) => {
